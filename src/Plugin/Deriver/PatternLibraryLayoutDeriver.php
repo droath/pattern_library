df890c2ea67a81afaee2ffde971c7e5985e1c942
Travis Tomka <underoathosx@gmail.com>
2018-01-21 20:39:55 -0700

    Use the container derivier interface for service dependency injection.

--- src/Plugin/Deriver/PatternLibraryLayoutDeriver.php
+++ src/Plugin/Deriver/PatternLibraryLayoutDeriver.php
@@ -4,21 +4,48 @@ namespace Drupal\pattern_library\Plugin\Deriver;
 
 use Drupal\Component\Plugin\Derivative\DeriverBase;
 use Drupal\Component\Plugin\PluginManagerInterface;
+use Drupal\Core\Plugin\Discovery\ContainerDeriverInterface;
 use Drupal\pattern_library\Plugin\Layout\PatternLayoutDefinition;
 use Drupal\pattern_library\Plugin\Layout\PatternLibraryLayout;
 use Drupal\pattern_library\Plugin\PatternInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
  * Creates a layout based on pattern definitions.
  */
-class PatternLibraryLayoutDeriver extends DeriverBase {
+class PatternLibraryLayoutDeriver extends DeriverBase implements ContainerDeriverInterface {
+
+  /**
+   * @var PluginManagerInterface
+   */
+  protected $patternLibraryManager;
+
+  /**
+   * Class constructor.
+   *
+   * @param $base_plugin_id
+   * @param PluginManagerInterface $pattern_library_manager
+   */
+  public function __construct($base_plugin_id, PluginManagerInterface $pattern_library_manager) {
+    $this->patternLibraryManager = $pattern_library_manager;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container, $base_plugin_id) {
+    return new static(
+      $base_plugin_id,
+      $container->get('plugin.manager.pattern_library')
+    );
+  }
 
   /**
    * {@inheritdoc}
    */
   public function getDerivativeDefinitions($base_plugin_definition) {
     /** @var \Drupal\pattern_library\Plugin\Pattern $pattern **/
-    foreach ($this->getPatternLibraryManager()->getDefinitionInstances() as $plugin_id => $pattern) {
+    foreach ($this->patternLibraryManager->getDefinitionInstances() as $plugin_id => $pattern) {
       if (!$pattern->validate()) {
         continue;
       }
@@ -63,13 +90,4 @@ class PatternLibraryLayoutDeriver extends DeriverBase {
     return $regions;
   }
 
-  /**
-   * Get pattern library manager.
-   *
-   * @return PluginManagerInterface
-   *   The pattern library manager.
-   */
-  protected function getPatternLibraryManager() {
-    return \Drupal::service('plugin.manager.pattern_library');
-  }
 }
