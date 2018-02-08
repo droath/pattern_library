7d4407071bfb4fe3a2ba2a4694243b19a9a5176d
Travis Tomka <underoathosx@gmail.com>
2018-02-08 15:55:03 -0700

    Add a boolean modifier type.

---
+++ src/Plugin/PatternModifierType/Boolean.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Drupal\pattern_library\Plugin\PatternModifierType;
+
+use Drupal\pattern_library\Annotation\PatternModifierType;
+use Drupal\pattern_library\Plugin\PatternModifierTypeBase;
+use Drupal\pattern_library\Plugin\PatternModifierTypeInterface;
+
+/**
+ * Define boolean pattern modifier type.
+ *
+ * @PatternModifierType(
+ *   id = "boolean"
+ * )
+ */
+class Boolean extends PatternModifierTypeBase implements PatternModifierTypeInterface {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function render() {
+    return [
+      '#type' => 'checkbox',
+    ] + parent::render();
+  }
+}
